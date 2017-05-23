49e8aa0c213ea43561c4dc8a005e845137ec18b2
Katie Leonard <kleonard@newrelic.com>
2017-05-23 14:59:58 -0700

    ruby code challenge.

---
+++ challenges/170523-school-roster.rb
@@ -0,0 +1,110 @@
+class School
+  attr_accessor :student_collection
+
+  def initialize
+    @student_collection = {}
+  end
+
+  def students(grade)
+    return [] unless student_collection[grade]
+    student_collection[grade].sort
+  end
+
+  def add(name, grade)
+    if grade_list = student_collection[grade]
+      grade_list << name
+    else
+      student_collection[grade] = [name]
+    end
+  end
+
+  def students_by_grade
+    student_collection.sort.collect do |key, students|
+      unless students(key).empty?
+        {grade: key, students: students(key)}
+      end
+    end
+  end
+end
+
+require 'minitest/autorun'
+
+class SchoolTest < Minitest::Test
+  def test_empty_grade
+    school = School.new
+    expected = []
+    assert_equal expected, school.students(1)
+  end
+
+  def test_add_student
+    school = School.new
+    assert school.add('Aimee', 2)
+    expected = ['Aimee']
+    assert_equal expected, school.students(2)
+  end
+
+  def test_add_students_to_different_grades
+    school = School.new
+    school.add('Aimee', 3)
+    school.add('Beemee', 7)
+    assert_equal ['Aimee'], school.students(3)
+    assert_equal ['Beemee'], school.students(7)
+  end
+
+  def test_grade_with_multiple_students
+    school = School.new
+    grade    = 6
+    students = %w(Aimee Beemee Ceemee)
+    students.each { |student| school.add(student, grade) }
+    assert_equal students, school.students(grade)
+  end
+
+  def test_grade_with_multiple_students_sorts_correctly
+    school = School.new
+    grade    = 6
+    students = %w(Beemee Aimee Ceemee)
+    students.each { |student| school.add(student, grade) }
+    expected = students.sort
+    assert_equal expected, school.students(grade)
+  end
+
+  def test_empty_students_by_grade
+    school = School.new
+    expected = []
+    assert_equal expected, school.students_by_grade
+  end
+
+  def test_students_by_grade
+    school = School.new
+    grade    = 6
+    students = %w(Beemee Aimee Ceemee)
+    students.each { |student| school.add(student, grade) }
+    expected = [{ grade: grade, students: students.sort }]
+    assert_equal expected, school.students_by_grade
+  end
+
+  def test_students_by_grade_sorted
+    school = School.new
+    everyone.each do |grade|
+      grade[:students].each { |student| school.add(student, grade[:grade]) }
+    end
+    expected = everyone_sorted
+    assert_equal expected, school.students_by_grade
+  end
+
+  def everyone
+    [
+      { grade: 3, students: %w(Deemee Eeemee) },
+      { grade: 1, students: %w(Effmee Geemee) },
+      { grade: 2, students: %w(Aimee Beemee Ceemee) }
+    ]
+  end
+
+  def everyone_sorted
+    [
+      { grade: 1, students: %w(Effmee Geemee) },
+      { grade: 2, students: %w(Aimee Beemee Ceemee) },
+      { grade: 3, students: %w(Deemee Eeemee) }
+    ]
+  end
+end
